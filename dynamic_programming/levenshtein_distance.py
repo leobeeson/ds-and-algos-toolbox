940c2fb8572f49a2d15491cb803b2b9f48e45366
Francis Beeson <leobeeson@gmail.com>
2022-08-21 19:32:29 +0100

    Implement levenshtein algorithm

--- dynamic_programming/levenshtein_distance.py
+++ dynamic_programming/levenshtein_distance.py
@@ -1,9 +1,7 @@
 import numpy
 
-token_1 = "hello"
-token_2 = "kelm"
 
-
+# Source: https://blog.paperspace.com/implementing-levenshtein-distance-word-autocomplete-autocorrect/
 def levenshtein_distance(token_1: str, token_2: str) -> int:
     distances = numpy.zeros((len(token_1) + 1, len(token_2) + 1))
 
@@ -13,8 +11,18 @@ def levenshtein_distance(token_1: str, token_2: str) -> int:
     for j in range(len(token_2) + 1):
         distances[0, j] = j
 
+    for i in range(1, len(token_1) + 1):
+        for j in range(1, len(token_2) + 1):
+            if token_1[i - 1] == token_2[j - 1]:
+                distances[i, j] = distances[i - 1, j -1]
+            else:
+                north_east = distances[i - 1, j - 1]
+                east = distances[i, j - 1]
+                north = distances[i - 1, j]
+                distances[i, j] = min(north_east, east, north) + 1
+
     print_distances(distances, len(token_1), len(token_2))
-    return 0
+    return distances[len(token_1), len(token_2)]
 
 
 def print_distances(distances_matrix: numpy.ndarray, row_length: int, column_length: int) -> None:
@@ -24,4 +32,7 @@ def print_distances(distances_matrix: numpy.ndarray, row_length: int, column_len
         print()
 
 
-levenshtein_distance(token_1, token_2)
+if __name__ == "__main__":
+    token_1 = "kelm"
+    token_2 = "hello"
+    levenshtein_distance(token_1, token_2)
